f1b11094dee305a9ae271c54576d6a204c4a5fbf
baonguyenbxl <69773267+baonguyenbxl@users.noreply.github.com>
2022-05-30 20:11:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,13 +23,13 @@ function App ()
   const [ texte3, setTexte3 ] = useState( data.texte.test3 );
   // eslint-disable-next-line
   const jsx = ( <div className="rowmain">
-    <div className="columna" style={ { backgroundColor: "#aaa" } }>
+    <div className="columna" style={ { backgroundColor: "#FFFFFF" } }>
       <ImageDiv valeur={ data.findmeforjob.fmfj } field="fmfj" />
       <InputDiv valeur={ texte } cbOnChange={ setTexte } field="test" />
       <PasswordDiv valeur={ texte2 } cbOnChange={ setTexte2 } />
       <button onClick={ () => { console.log( { papa: texte, filsa: texte2, filsb: texte3 } ) } }>Click me</button>
     </div>
-    <div className="columnb" style={ { backgroundColor: "#bbb" } }>
+    <div className="columnb" style={ { backgroundColor: "#000000" } }>
       <DateDiv />
 
       <TexteDiv valeurLink={ data.findmeforjob.fmfj } valeurTarget="_blank" valeur={ texte } />
@@ -38,7 +38,7 @@ function App ()
       <label>{ texte2 }</label><br />
       <label>{ texte3 }</label><br />
     </div>
-    <div className="columna" style={ { backgroundColor: "#ccc" } }>
+    <div className="columna" style={ { backgroundColor: "#FFFFFF" } }>
       <InputDiv valeur={ texte2 } cbOnChange={ setTexte2 } field="test2" />
       <InputDiv valeur={ texte3 } cbOnChange={ setTexte3 } field="test3" />
     </div>
